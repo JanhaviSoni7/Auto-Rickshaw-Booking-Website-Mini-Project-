464c59e9ad69b4537ce1767f983419b76cb66242
JanhaviSoni7 <121169112+JanhaviSoni7@users.noreply.github.com>
2024-04-09 14:57:19 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+$hostName = "localhost";
+$dbUser = "root";
+$dbPassword = "1234";
+$dbName = "login_register";
+$conn = new mysqli($hostName, $dbUser, $dbPassword, $dbName, 3307) or die("not connected");
+
+?>
